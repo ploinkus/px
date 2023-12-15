d5053fde0b5ca837daf3b7efcdef64f6a27e9c59
ploinkus <120677943+ploinkus@users.noreply.github.com>
2023-12-15 14:19:04 -0500

    function that makes the time.sleep() method more readable
    readable waiting for python

---
+++ bettertime.py
@@ -0,0 +1,14 @@
+import time
+
+def wait_seconds(x):
+  time.sleep(x)
+#this makes the time.sleep method into a function that is easier to read.
+
+#ex:
+  print('waiting 2 seconds with the normal time.sleep() function:')
+  time.sleep(2)
+  print('done')
+#the time.sleep is less readable than the wait_seconds() function
+  print('waiting 2 seconds with the normal time.sleep() function:')
+  wait_seconds(2)
+  print('done')
